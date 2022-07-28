ce95c87b266b3b9be818f23503e8cdb95774bef6
David Zwart <davidzwa@gmail.com>
2022-07-29 00:25:56 +0200

    add assets, bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Game from "./components/Game";
+import { GameBg } from "./components/GameBg";
 
 function App() {
   return (
     <div className="App">
+      <GameBg />
       <h1 style={{ textAlign: "center" }}>Puffermote v1.9.63</h1>
       <br />
       <Game />
