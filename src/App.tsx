248cf072b7189777c50e59427e3de03221dd8e5e
David Zwart <davidzwa@gmail.com>
2022-07-29 00:02:00 +0200

    Setup p5 canvas

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>Puffermote v1.9.63</h1>
+      <br />
+      <Game />
+    </div>
+  );
+}
+
+export default App;
